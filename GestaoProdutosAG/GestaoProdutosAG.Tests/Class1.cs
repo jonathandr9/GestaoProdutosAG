95a003c43b9ef1db921f39324297860be0c25c74
Jonathan Dias <jonathandias@rumosolucoes.com>
2023-02-06 22:03:57 -0300

    Innitial archteture

---
+++ GestaoProdutosAG/GestaoProdutosAG.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GestaoProdutosAG.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ GestaoProdutosAG/GestaoProdutosAG/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoProdutosAG.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
